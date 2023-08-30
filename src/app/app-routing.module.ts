fa4f5f9c0353ba0b93e5802f296bb4ded2305959
László Farkas <laszlo.farkas@brandwatch.com>
2023-08-30 15:38:36 +0200

    Books and details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { BooksComponent } from './books/books.component';
+import { BookDetailsComponent } from './book-details/book-details.component';
 
 const routes: Routes = [
-  { path: '**', component: HomeComponent }
+  {
+    path: 'book',
+    children: [
+      { path: ':id', component: BookDetailsComponent },
+      { path: '**', component: BooksComponent },
+    ],
+  },
+  { path: '**', component: HomeComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { BookTileComponent } from './book-tile/book-tile.component';
+import { BooksComponent } from './books/books.component';
+import { BookDetailsComponent } from './book-details/book-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    BookTileComponent
+    BookTileComponent,
+    BooksComponent,
+    BookDetailsComponent
   ],
   imports: [
     BrowserModule,
