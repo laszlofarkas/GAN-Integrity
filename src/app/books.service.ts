e3486d2e74d6371043d7a90cf8a9035b2fe5e8e1
László Farkas <laszlo.farkas@brandwatch.com>
2023-08-30 10:43:23 +0200

    BooksService

---
+++ src/app/books.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+type Book = {
+  id: number;
+  author: string;
+  title: string;
+  description: string;
+  releaseDate: Date;
+};
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BooksService {
+  private books: Book[] = [];
+
+  constructor() {
+    for (let i = 0; i < 10; i++) {
+      const date = new Date('2023-01-01');
+      date.setMonth(i);
+      this.books.push({
+        id: i,
+        author: `author ${i}`,
+        title: `title ${i}`,
+        description: `Description ${i}`,
+        releaseDate: date,
+      })
+    }
+  }
+
+  getBooks(): Observable<Book[]> {
+    return of(this.books);
+  }
+}
